a40fff108b02bed29389cb92f68d0a3e26170568
TishCom <tishaev.kolya@mail.ru>
2026-02-21 16:15:44 +0300

    PrataLesson2

---
+++ Language C. S Prata/Lesson2/6_Variables.c
@@ -0,0 +1,35 @@
+/*
+ * 6_Variables.c
+ * 
+ * Copyright 2026 tisha <tisha@DESKTOP-H2QK95F>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int toes;
+	toes = 10;
+	printf("toes = %d, 2 x toes = %d, toes^2 = %d", toes, toes * 2, toes * toes);
+	
+	return 0;
+}
+
---
+++ Language C. S Prata/Lesson2/8_FunctionCall.c
@@ -0,0 +1,49 @@
+/*
+ * 8_FunctionCall.c
+ * 
+ * Copyright 2026 tisha <tisha@DESKTOP-H2QK95F>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+void one_three(void);
+void two(void);
+
+int main(int argc, char **argv)
+{
+	printf("Start:\n");
+	one_three();
+	printf("order!");
+	
+	return 0;
+}
+
+void one_three(void)
+{
+	printf("One\n");
+	two();
+	printf("Tree\n");
+}
+
+void two(void)
+{
+	printf("Two\n");
+}
