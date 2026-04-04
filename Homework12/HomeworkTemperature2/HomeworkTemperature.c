b97c8acfab4ef27657a0f5657a297e1ae3183b8a
TishCom <tishaev.kolya@mail.ru>
2026-04-04 20:48:17 +0300

    Homework12

---
+++ Homework12/HomeworkTemperature2/HomeworkTemperature.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "Sensor_Temperature.h"
+
+#define SIZE 200
+
+void help(void);
+void fileParam(void);
+void monthParam(void);
+
+int main(int argc, char **argv)
+{
+	sensor_readings info[SIZE] ={0};
+	int rez=0;
+
+    while ( (rez = getopt(argc,argv,"hf:m:")) != -1)
+    {
+        switch (rez)
+        {
+            case 'h': help(); break;
+            case 'f': fileParam(); break;
+            case 'm': monthParam(); break;
+            case '?': printf("Error found!\n");
+        }
+    }
+	
+	return 0;
+}
+
+void help(void)
+{
+	printf("Help.\n");
+	printf("Description of the application functionality.\n");
+	printf("Parameters:\n");
+	printf("1)h - help\n");
+	printf("2)f - file source\n");
+	printf("3)m - month\n");
+	printf("4)other - error\n");
+}
+
+void fileParam(void)
+{
+	printf("File - \"f%s\".\n", optarg);
+}
+
+void monthParam(void)
+{
+	printf("Month - \"%s\".\n", optarg);
+}
